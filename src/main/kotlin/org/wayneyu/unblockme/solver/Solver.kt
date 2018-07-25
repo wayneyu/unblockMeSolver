a6bc9ff72cbb77c21fbb96aa27052837c4009cd6
Wayne Yu <wingwa.yu@gmail.com>
2018-07-25 01:38:40 -0400

    Add test cases to solve 6x6 board

--- src/main/kotlin/org/wayneyu/unblockme/solver/Solver.kt
+++ src/main/kotlin/org/wayneyu/unblockme/solver/Solver.kt
@@ -3,25 +3,23 @@ package org.wayneyu.unblockme.solver
 import org.wayneyu.unblockme.solver.model.Board
 import java.util.*
 
-class Solver {
+class Solver(val xEndLoc: Int = 1) {
 
     fun solve(board: Board): List<Board> {
-        val shortestPath = BFS.shortestPath(BoardNode(board))
+        val shortestPath = BFS.shortestPath(BoardNode(board, this))
         return shortestPath.map{ (it as BoardNode).board }
     }
 
-    companion object {
-        fun isSolved(board: Board): Boolean =
-                board.redBar.xStart == 1 && board.redBar.yStart == (board.ySize - 2)
-    }
+    fun isSolved(board: Board): Boolean =
+            board.redBar.xStart == xEndLoc && board.redBar.yStart == (board.ySize - 2)
 
 }
 
-data class BoardNode(val board: Board) : Node {
+data class BoardNode(val board: Board, val solver: Solver) : Node {
     override val neighbors: Set<Node>
-        get() = board.neighbors.mapTo(HashSet<Node>()){ BoardNode(it) }
+        get() = board.neighbors.mapTo(HashSet<Node>()){ BoardNode(it, solver) }
 
-    override fun isEnd(): Boolean = Solver.isSolved(board)
+    override fun isEnd(): Boolean = solver.isSolved(board)
 }
 
 fun main(args: Array<String>) {
--- src/test/kotlin/org/wayneyu/unblockme/solver/SolverTest.kt
+++ src/test/kotlin/org/wayneyu/unblockme/solver/SolverTest.kt
@@ -3,7 +3,6 @@ package org.wayneyu.unblockme.solver
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.xit
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 import org.wayneyu.unblockme.solver.model.Bar
@@ -16,36 +15,55 @@ import kotlin.test.assertTrue
 @RunWith(JUnitPlatform::class)
 class SolverTest : Spek({
 
-    describe("should solve simple 4x4 board") {
+    val fixture = Solver()
 
-        val board = Board(4, 4, listOf(Bar(1, 0, 1, 2), Bar(1, 2, 0, 2), Bar(3, 0, 1, 2)))
-        /*
-        |----|
-        |001-|
-        |--1-|
-        |22--|
-        */
-        val fixture = Solver()
+    val board4x4 = Board(4, 4, listOf(Bar(1, 0, 1, 2), Bar(1, 2, 0, 2), Bar(3, 0, 1, 2)))
+    /*
+    |----|
+    |001-
+    |--1-|
+    |22--|
+    */
+
+    describe("solver") {
+        it("should mark as solved") {
+            /*
+            |----|
+            |--00
+            |--1-|
+            |--1-|
+            */
+            val finished = Board(4, 4, listOf(Bar(1, 2, 1, 2), Bar(2, 2, 0, 2)))
+            assertTrue(fixture.isSolved(finished))
+            assertFalse(fixture.isSolved(board4x4))
+        }
+    }
+
+    describe("should solve simple 4x4 board") {
 
         it("should solve board in two moves") {
             /*
             |----|
-            |--00|
+            |--00
             |--1-|
             |221-|
             */
             val expected = listOf(
-                    board,
-                    board.move(1, Move(1, 0)),
-                    board.move(1, Move(1, 0))?.move(0, Move(2, 1)))
+                    board4x4,
+                    board4x4.move(1, Move(1, 0)),
+                    board4x4.move(1, Move(1, 0))?.move(0, Move(2, 1)))
 
-            assertEquals(expected, fixture.solve(board))
+            assertEquals(expected, fixture.solve(board4x4))
         }
+
+    }
+
+    describe("should solve 5x5 board") {
 
         it("should solve board in four moves") {
             /*
             |--31-|
-            |0031-|
+            |0031-
             |--31-|
             |-22--|
             |-----|
@@ -56,20 +74,41 @@ class SolverTest : Spek({
                     3 to Move(2, 0),
                     1 to Move(2, 0),
                     0 to Move(3, 1))))
+            val actual = fixture.solve(board5x5)
 
-            assertEquals(expected, fixture.solve(board5x5))
+            assertEquals(expected.size, actual.size)
         }
+    }
 
-        it("should mark as solved") {
+    describe("should solve 6x6 board") {
+
+        xit("should solve board in 15 moves") {
             /*
-            |----|
-            |--00|
-            |--1-|
-            |--1-|
+            |--2344|
+            |11235-|
+            |60035-
+            |678899|
+            |-7A--C|
+            |--ABBC|
             */
-            val finished = Board(4, 4, listOf(Bar(1, 2, 1, 2), Bar(2, 2, 0, 2)))
-            assertTrue(Solver.isSolved(finished))
-            assertFalse(Solver.isSolved(board))
+            val board6x6 = Board(6, 6, listOf(
+                    Bar(2, 1, 1, 2),
+                    Bar(1, 0, 1, 2),
+                    Bar(0, 2, 0, 2),
+                    Bar(0, 3, 0, 3),
+                    Bar(0, 4, 1, 2),
+                    Bar(1, 4, 0, 2),
+                    Bar(2, 0, 0, 2),
+                    Bar(3, 1, 0, 2),
+                    Bar(3, 2, 1, 2),
+                    Bar(3, 4, 1, 2),
+                    Bar(4, 2, 0, 2),
+                    Bar(5, 3, 1, 2),
+                    Bar(4, 5, 0, 2)))
+
+            println(board6x6.layout + "\n")
+            Solver(xEndLoc = 2).solve(board6x6).forEach { println(it.layout + "\n") }
+            assertEquals(15, Solver(xEndLoc = 2).solve(board6x6).size)
         }
     }
 
